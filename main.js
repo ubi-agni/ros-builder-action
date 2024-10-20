a00bfffc88d663377e60abd7f368c699aa357686
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2024-10-21 00:09:18 +0200

    build action: succeed on timeout

--- main.js
+++ main.js
@@ -45,8 +45,10 @@ child.on('exit', function (exit_code, signal) {
   exit_code = exit_code !== null ? exit_code : 143
   const suffix = exit_code == expect ? ' (as expected)' : ' != ' + expect
   const msg = 'Process finished with code ' + exit_code + suffix
-  exit_code == expect ? core.debug(msg) : core.warning(msg)
-  process.exit(exit_code == expect ? 0 : 1)
+
+  const success = exit_code == expect || exit_code == 143
+  success ? core.debug(msg) : core.warning(msg)
+  process.exit(success ? 0 : 1)
 })
 
 // cancel build after given timout (github default: 6h - 20min slack)
