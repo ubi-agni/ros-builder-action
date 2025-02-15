82bdec936cc338c325d609e23eab0b5892fba81e
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2025-02-15 11:44:58 +0100

    import.sh: fold and colorize output

--- reprepro/import.sh
+++ reprepro/import.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+DIR_THIS="$(dirname "${BASH_SOURCE[0]}")"
+SRC_PATH="$(realpath "$DIR_THIS/../src")"
+
+# shellcheck source=src/util.sh
+source "$SRC_PATH/util.sh"
+
 if [ -r ~/.reprepro.env ]; then
 	# shellcheck disable=SC1090
 	. ~/.reprepro.env
@@ -25,21 +31,23 @@ function import {
 
 	# Import sources
 	if [ "$arch" == "amd64" ]; then
-		printf "\nImporting source packages\n"
+		ici_start_fold "$(ici_colorize BLUE BOLD "Importing source packages")"
 		for f in "$INCOMING_DIR"/*.dsc; do
 			[ -f "$f" ] || break  # Handle case of no files found
 			echo "${f#"$INCOMING_DIR/"}"
 			reprepro includedsc "$distro" "$f" | filter
 		done
+		ici_end_fold
 	fi
 
 	# Import packages
-	printf "\nImporting binary packages\n"
+	ici_start_fold "$(ici_colorize BLUE BOLD "Importing binary packages")"
 	for f in "$INCOMING_DIR"/*.deb; do
 		[ -f "$f" ] || break  # Handle case of no files found
 		echo "${f#"$INCOMING_DIR/"}"
 		reprepro -A "$arch" includedeb "$distro" "$f" | filter
 	done
+	ici_end_fold
 
 	# Save log files
 	mkdir -p "log/${distro%-testing}.$arch"
@@ -49,7 +57,7 @@ function import {
 	(cd "$INCOMING_DIR" || exit 1; rm -f ./*.log ./*.deb ./*.dsc ./*.tar.gz ./*.tar.xz ./*.changes ./*.buildinfo)
 
 	# Rename, Import, and Cleanup ddeb files (if existing)
-	printf "\nImporting debug packages\n"
+	ici_start_fold "$(ici_colorize BLUE BOLD "Importing debug packages")"
 	for f in "$INCOMING_DIR"/*.ddeb; do
 		[ -f "$f" ] || break  # Handle case of no files found
 		echo "${f#"$INCOMING_DIR/"}"
@@ -59,9 +67,9 @@ function import {
 		reprepro -A "$arch" -C main-dbg includedeb "$distro" "${f}.deb" | filter
 	done
 	(cd "$INCOMING_DIR" || exit 1; rm ./*.deb)
+	ici_end_fold
 
-	printf "\nExporting\n"
-	reprepro export "$distro"
+	ici_cmd reprepro export "$distro"
 
 	# Merge local.yaml into ros-one.yaml
 	cat "$INCOMING_DIR/local.yaml" >> "ros-one.yaml"
@@ -73,7 +81,7 @@ function import {
 
 # Download debs artifact(s)
 if [ "$(ls -A "$INCOMING_DIR")" ]; then
-	echo "Importing existing files from incoming directory"
+	ici_color_output CYAN BOLD "Importing existing files from incoming directory"
 	# shellcheck disable=SC2153 # DISTRO and ARCH might be unset
 	import "$DISTRO" "$ARCH"
 else
@@ -84,8 +92,8 @@ else
 	# Retrieve names of artifacts in that workflow run
 	artifacts=$(gh api -X GET "/repos/$REPO/actions/artifacts" | jq --raw-output ".artifacts[] | select(.workflow_run.id == $RUN_ID) | .name")
 	for a in $artifacts; do
-		echo "Fetching artifact \"$a\" from https://github.com/$REPO/actions/runs/$RUN_ID"
-		gh --repo "$REPO" run download --name "$a" --dir "$INCOMING_DIR" "$RUN_ID" || continue
+		msg="Fetching artifact \"$a\" from https://github.com/$REPO/actions/runs/$RUN_ID"
+		ici_timed "$(ici_colorize CYAN BOLD "$msg")" gh --repo "$REPO" run download --name "$a" --dir "$INCOMING_DIR" "$RUN_ID"
 
 		# parse distro and arch from artifact name <distro>-<arch>-debs
 		if [[ $a =~ ([^-]+)-([^-]+)(-debs)? ]]; then
