2df9d0689f791c90beee58ba70414ed5d4ac3cc5
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2025-09-02 15:37:22 +0200

    Detect/Report deploy failure

--- reprepro/deploy.js
+++ reprepro/deploy.js
@@ -1,7 +1,10 @@
+#!/usr/bin/env -S node
+
 'use strict'
 const core = require('@actions/core');
 const EventSource = require('eventsource');
 
+const FAILURE_MSG = 'Failed with return code ';
 const url = core.getInput('url');
 
 const evtSource = new EventSource(url);
@@ -9,7 +12,12 @@ const evtSource = new EventSource(url);
 evtSource.onmessage = (event) => {
 	if (event.data === '')
 		evtSource.close();
-	else
+	else if (event.data.startsWith(FAILURE_MSG)) {
+		core.warning(event.data);
+		evtSource.close();
+		const err = event.data.split(FAILURE_MSG)[1];
+		process.exit(err);
+	} else
 		console.log(event.data)
 };
 
