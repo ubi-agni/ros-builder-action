13963b9b0bd865ee30f83b081fa7b2aeb42ae7f2
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2025-09-03 05:57:56 +0200

    Prepare fastapi.venv if not existing

--- reprepro/server.sh
+++ reprepro/server.sh
@@ -4,12 +4,18 @@ DIR_THIS="$(dirname "${BASH_SOURCE[0]}")"
 
 # shellcheck disable=SC1090
 source ~/.reprepro.env
+if [ ! -d ~/fastapi.venv ]; then
+	python3 -m venv ~/fastapi.venv
+	pip install "fastapi[standard]" starlette sse_starlette colorama
+fi
 # shellcheck disable=SC1090
-source ~/venv/bin/activate
+source ~/fastapi.venv/bin/activate
 
 # Add option "allow-preset-passphrase" to .gnupg/gpg-agent.conf before calling gpg-preset-passphrase!
 /usr/lib/gnupg2/gpg-preset-passphrase -v --preset "$GPG_KEY" <<EOF
 $GPG_PW
 EOF
+unset GPG_KEY
+unset GPG_PW
 
 fastapi run "$DIR_THIS/server.py"
